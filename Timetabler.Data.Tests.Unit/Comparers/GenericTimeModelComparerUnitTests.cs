9ea741610c0eb52f32657ffc6aceda42589a3310
Will Salt <forestpines@gmail.com>
2019-06-21 21:57:36 +0100

    #13 A few more unit tests where relevant

--- Timetabler.Data.Tests.Unit/Comparers/GenericTimeModelComparerUnitTests.cs
+++ Timetabler.Data.Tests.Unit/Comparers/GenericTimeModelComparerUnitTests.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Tests.Utility.Extensions;
 using Tests.Utility.Providers;
 using Timetabler.Data.Comparers;
 using Timetabler.Data.Display;
@@ -61,6 +58,102 @@ namespace Timetabler.Data.Tests.Unit.Comparers
             int testOutput = testObject.Compare(testParam0, testParam1);
 
             Assert.AreEqual(0, testOutput);
+        }
+
+        [TestMethod]
+        public void GenericTimeModelComparerClassCompareMethodReturnsMinusOneWhenFirstParameterIsNullAndSecondParameterIsValidObject()
+        {
+            GenericTimeModelComparer testObject = GenericTimeModelComparer.Default;
+            GenericTimeModel testParam0 = null;
+            GenericTimeModel testParam1 = new GenericTimeModel { ActualTime = _rnd.NextTimeOfDay() };
+
+            int testOutput = testObject.Compare(testParam0, testParam1);
+
+            Assert.AreEqual(-1, testOutput);
+        }
+
+        [TestMethod]
+        public void GenericTimeModelComparerClassCompareMethodReturnsMinusOneWhenFirstParameterHasNullActualTimePropertyAndSecondParameterIsValidObject()
+        {
+            GenericTimeModelComparer testObject = GenericTimeModelComparer.Default;
+            GenericTimeModel testParam0 = new GenericTimeModel { ActualTime = null };
+            GenericTimeModel testParam1 = new GenericTimeModel { ActualTime = _rnd.NextTimeOfDay() };
+
+            int testOutput = testObject.Compare(testParam0, testParam1);
+
+            Assert.AreEqual(-1, testOutput);
+        }
+
+        [TestMethod]
+        public void GenericTimeModelComparerClassCompareMethodReturnsOneWhenFirstParameterIsValidObjectAndSecondParameterIsNull()
+        {
+            GenericTimeModelComparer testObject = GenericTimeModelComparer.Default;
+            GenericTimeModel testParam0 = new GenericTimeModel { ActualTime = _rnd.NextTimeOfDay() };
+            GenericTimeModel testParam1 = null;
+
+            int testOutput = testObject.Compare(testParam0, testParam1);
+
+            Assert.AreEqual(1, testOutput);
+        }
+
+        [TestMethod]
+        public void GenericTimeModelComparerClassCompareMethodReturnsOneWhenFirstParameterIsValidObjectAndSecondParameterHasNullActualTimeProperty()
+        {
+            GenericTimeModelComparer testObject = GenericTimeModelComparer.Default;
+            GenericTimeModel testParam0 = new GenericTimeModel { ActualTime = _rnd.NextTimeOfDay() };
+            GenericTimeModel testParam1 = new GenericTimeModel { ActualTime = null };
+
+            int testOutput = testObject.Compare(testParam0, testParam1);
+
+            Assert.AreEqual(1, testOutput);
+        }
+
+        [TestMethod]
+        public void GenericTimeModelComparerClassCompareMethodReturnsZeroWhenFirstParameterActualTimePropertyIsEqualToSecondParameterActualTimeProperty()
+        {
+            GenericTimeModelComparer testObject = GenericTimeModelComparer.Default;
+            GenericTimeModel testParam0 = new GenericTimeModel { ActualTime = _rnd.NextTimeOfDay() };
+            GenericTimeModel testParam1 = new GenericTimeModel { ActualTime = testParam0.ActualTime.Copy() };
+
+            int testOutput = testObject.Compare(testParam0, testParam1);
+
+            Assert.AreEqual(0, testOutput);
+        }
+
+        [TestMethod]
+        public void GenericTimeModelComparerClassCompareMethodReturnsZeroWhenFirstParameterActualTimePropertyIsSameAsSecondParameterActualTimeProperty()
+        {
+            GenericTimeModelComparer testObject = GenericTimeModelComparer.Default;
+            GenericTimeModel testParam0 = new GenericTimeModel { ActualTime = _rnd.NextTimeOfDay() };
+            GenericTimeModel testParam1 = new GenericTimeModel { ActualTime = testParam0.ActualTime };
+
+            int testOutput = testObject.Compare(testParam0, testParam1);
+
+            Assert.AreEqual(0, testOutput);
+        }
+
+        [TestMethod]
+        public void GenericTimeModelComparerClassCompareMethodReturnsMinusOneWhenFirstParameterActualTimePropertyIsBeforeSecondParameterActualTimeProperty()
+        {
+            GenericTimeModelComparer testObject = GenericTimeModelComparer.Default;
+            GenericTimeModel testParam0 = new GenericTimeModel { ActualTime = _rnd.NextTimeOfDayBefore(new TimeOfDay(86398)) };
+            GenericTimeModel testParam1 = new GenericTimeModel { ActualTime = _rnd.NextTimeOfDayAfter(testParam0.ActualTime) };
+
+            int testOutput = testObject.Compare(testParam0, testParam1);
+
+            Assert.AreEqual(-1, testOutput);
+        }
+
+        [TestMethod]
+        public void GenericTimeModelComparerClassCompareMethodReturnsOneWhenFirstParameterActualTimePropertyIsAfterSecondParameterActualTimeProperty()
+        {
+            GenericTimeModelComparer testObject = GenericTimeModelComparer.Default;
+            GenericTimeModel testParam0 = new GenericTimeModel { ActualTime = _rnd.NextTimeOfDayAfter(new TimeOfDay(2)) };
+            GenericTimeModel testParam1 = new GenericTimeModel { ActualTime = _rnd.NextTimeOfDayBefore(testParam0.ActualTime) };
+
+            int testOutput = testObject.Compare(testParam0, testParam1);
+
+            Assert.AreEqual(1, testOutput);
         }
     }
 }
---
+++ Timetabler.Data.Tests.Unit/Comparers/TrainSegmentModelComparerUnitTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Timetabler.Data.Comparers;
+using Timetabler.Data.Display;
+
+namespace Timetabler.Data.Tests.Unit.Comparers
+{
+    [TestClass]
+    public class TrainSegmentModelComparerUnitTests
+    {
+        [TestMethod]
+        public void TrainSegmentModelComparerClassCompareMethodReturnsObjectWithItem1EqualToZeroIfBothParametersAreNull()
+        {
+            TrainSegmentModelComparer testObject = new TrainSegmentModelComparer(new List<LocationDisplayModel>());
+
+            Tuple<int, TrainSegmentModel> testOutput = testObject.Compare(null, null);
+
+            Assert.AreEqual(0, testOutput.Item1);
+        }
+
+        [TestMethod]
+        public void TrainSegmentModelComparerClassCompareMethodReturnsObjectWithItem2EqualToNullIfBothParametersAreNull()
+        {
+            TrainSegmentModelComparer testObject = new TrainSegmentModelComparer(new List<LocationDisplayModel>());
+
+            Tuple<int, TrainSegmentModel> testOutput = testObject.Compare(null, null);
+
+            Assert.IsNull(testOutput.Item2);
+        }
+    }
+}

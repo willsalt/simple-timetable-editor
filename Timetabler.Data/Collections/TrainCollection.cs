e189519174467427c5bf40b37d506d1c21845265
Will Salt <forestpines@gmail.com>
2019-07-10 21:49:12 +0100

    #99 A bit of refactoring to expose which train is selected in the graph, and notify consumers when that selection changes.

--- Timetabler.Data/Collections/TrainCollection.cs
+++ Timetabler.Data/Collections/TrainCollection.cs
@@ -8,13 +8,6 @@ namespace Timetabler.Data.Collections
     public class TrainCollection : BaseCollection<Train>
     {
         /// <summary>
-        /// Event handler delegate type for <see cref="Train"/>-related events.
-        /// </summary>
-        /// <param name="sender">The object which raised this event.</param>
-        /// <param name="e">The arguments to this event.</param>
-        public delegate void TrainEventHandler(object sender, TrainEventArgs e);
-
-        /// <summary>
         /// Event raised when a train is added to the collection.
         /// </summary>
         public event TrainEventHandler TrainAdd;
---
+++ Timetabler.Data/Events/TrainEventHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace Timetabler.Data.Events
+{
+    /// <summary>
+    /// Event handler delegate type for <see cref="Train"/>-related events.
+    /// </summary>
+    /// <param name="sender">The object which raised this event.</param>
+    /// <param name="e">The arguments to this event.</param>
+    public delegate void TrainEventHandler(object sender, TrainEventArgs e);
+}

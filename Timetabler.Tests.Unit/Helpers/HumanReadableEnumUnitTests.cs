3d5a93a7abce5d7fbb2a62f92c8573cd64ef458d
Will Salt <forestpines@gmail.com>
2019-11-25 21:47:12 +0000

    #142 Add FxCop to Timetabler.Tests.Unit

--- Timetabler.Tests.Unit/Helpers/HumanReadableEnumUnitTests.cs
+++ Timetabler.Tests.Unit/Helpers/HumanReadableEnumUnitTests.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections.Generic;
 using System.Drawing.Drawing2D;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Tests.Utility.Extensions;
 using Tests.Utility.Providers;
 using Timetabler.CoreData;

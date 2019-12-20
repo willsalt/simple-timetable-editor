3c8d2e0e0b2a141af15a756256b7075aa43821ff
Will Salt <forestpines@gmail.com>
2019-12-20 20:47:36 +0000

    #121 Decide against introucing caching for ToPdfRectangle() at this time without evidence.  Some minor changes.

--- Unicorn.Writer.Tests.Unit/Extensions/UniSizeExtensionsUnitTests.cs
+++ Unicorn.Writer.Tests.Unit/Extensions/UniSizeExtensionsUnitTests.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using Tests.Utility.Providers;
 using Unicorn.Interfaces;
 using Unicorn.Writer.Extensions;

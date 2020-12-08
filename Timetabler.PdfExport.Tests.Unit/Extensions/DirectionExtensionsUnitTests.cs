1e586cbf775b41157ae7e7c1cf158614559b288d
Will Salt <forestpines@gmail.com>
2020-12-08 21:48:37 +0000

    Issue #357 Directly-related unit tests

---
+++ Timetabler.PdfExport.Tests.Unit/Extensions/DirectionExtensionsUnitTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tests.Utility.Providers;
+using Timetabler.CoreData;
+using Timetabler.PdfExport.Extensions;
+
+namespace Timetabler.PdfExport.Tests.Unit.Extensions
+{
+    [TestClass]
+    public class DirectionExtensionsUnitTests
+    {
+        private static readonly Random _rnd = RandomProvider.Default;
+
+#pragma warning disable CA1707 // Identifiers should not contain underscores
+
+        [TestMethod]
+        public void DirectionExtensionsClass_OppositeMethod_ReturnsUp_IfParameterEqualsDown()
+        {
+            Direction testParam = Direction.Down;
+
+            Direction testOutput = testParam.Opposite();
+
+            Assert.AreEqual(Direction.Up, testOutput);
+        }
+
+        [TestMethod]
+        public void DirectionExtensionsClass_OppositeMethod_ReturnsDown_IfParameterEqualsUp()
+        {
+            Direction testParam = Direction.Up;
+
+            Direction testOutput = testParam.Opposite();
+
+            Assert.AreEqual(Direction.Down, testOutput);
+        }
+
+#pragma warning disable CA5394 // Do not use insecure randomness
+
+        [TestMethod]
+        public void DirectionExtensionsClass_OppositeMethod_ReturnsValueEqualToParameter_IfParameterIsNotAValidDirectionValue()
+        {
+            Direction testParam;
+            do
+            {
+                testParam = (Direction)_rnd.Next();
+            } while (testParam == Direction.Down || testParam == Direction.Up);
+            Direction expectedValue = testParam;
+
+            Direction testOutput = testParam.Opposite();
+
+            Assert.AreEqual(expectedValue, testOutput);
+        }
+
+#pragma warning restore CA5394 // Do not use insecure randomness
+#pragma warning restore CA1707 // Identifiers should not contain underscores
+
+    }
+}
--- Timetabler.PdfExport/Extensions/DirectionExtensions.cs
+++ Timetabler.PdfExport/Extensions/DirectionExtensions.cs
@@ -2,8 +2,16 @@
 
 namespace Timetabler.PdfExport.Extensions
 {
+    /// <summary>
+    /// Extension methods for the <see cref="Direction" /> enumeration type.
+    /// </summary>
     public static class DirectionExtensions
     {
+        /// <summary>
+        /// Given a <see cref="Direction" />, return the opposite direction.
+        /// </summary>
+        /// <param name="d">A <see cref="Direction" /> value.</param>
+        /// <returns>The opposite direction to the parameter.</returns>
         public static Direction Opposite(this Direction d)
         {
             switch (d)
